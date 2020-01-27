9d2257ba01ef3f55737bee9b052677d376316ec5
Fernando Mosquera <benitoss@gmail.com>
2020-01-27 17:16:40 -0600

    Fixed SNES DB9 Core by benitoss

--- SNES_MiSTer-master/sys/sys.tcl
+++ SNES_MiSTer-master/sys/sys.tcl
@@ -35,6 +35,23 @@ set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to IO_S*
 #============================================================
 # USER PORT
 #============================================================
+#set_location_assignment PIN_AE15 -to joy_down
+#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to joy_down
+#set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to joy_down
+#set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to joy_down
+
+#set_location_assignment PIN_AF17 -to USER_IO[6]
+#set_location_assignment PIN_AF15 -to USER_IO[5]
+#set_location_assignment PIN_AG16 -to USER_IO[4]
+#set_location_assignment PIN_AH11 -to USER_IO[3]
+#set_location_assignment PIN_AH12 -to USER_IO[2]
+#set_location_assignment PIN_AH9 -to USER_IO[1]
+#set_location_assignment PIN_AG11 -to USER_IO[0]
+#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to USER_IO[*]
+#set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to USER_IO[*]
+#set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to USER_IO[*]
+
+
 set_location_assignment PIN_AG16 -to splitter_select
 set_location_assignment PIN_AG11 -to db9_Select
 set_location_assignment PIN_AE15 -to joy_down_i
@@ -51,8 +68,10 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to joy_p6_i
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to joy_p9_i
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to joy_right_i
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to joy_up_i
-set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to joy_[*]
-
+set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to joy_*
+set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to joy_*
+set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to splitter_select
+set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to db9_Select
 
 #============================================================
 # SDIO_CD or SPDIF_OUT
